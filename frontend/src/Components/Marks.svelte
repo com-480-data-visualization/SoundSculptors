a6be6cc44a9642a4acc2ba02e3d9617b01895a54
alex <anithacan01@gmail.com>
2024-04-23 15:07:51 +0200

    frontend

---
+++ frontend/src/Components/Marks.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { geoPath, geoNaturalEarth1 } from  "d3";
+    export let  dataset  =  [];
+    const  projection  =  geoNaturalEarth1();
+    const  path  =  geoPath(projection);
+</script>
+
+{#each  dataset  as data}
+    <path  d={path(data)} />
+{/each}
