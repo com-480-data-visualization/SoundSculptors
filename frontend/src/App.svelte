fa1aaeef6057527bb0faf82cb677f0567191968d
Yasmine009 <yasmine.chaker@epfl.ch>
2024-04-24 16:37:05 +0200

    radar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,20 @@
 <script>
-import Map from './lib/map.svelte'
+import Radar from './lib/graph.svelte'
 import {markets, iso2CodesByCountryName} from './lib/markets'
 
-let selected;
-let colors = {}
-$: fetch("http://127.0.0.1:5000/music_similarity?country_code="+iso2CodesByCountryName[selected?.properties.name.toLowerCase()]).then(x => x.json()).then(x => colors = x).catch(err => console.log(err))
-$: console.log(colors)
+//let selected;
+//let colors = {}
+//$: fetch("http://127.0.0.1:5000/music_similarity?country_code="+iso2CodesByCountryName[selected?.properties.name.toLowerCase()]).then(x => x.json()).then(x => colors = x).catch(err => console.log(err))
+//$: console.log(colors)
 </script>
 
 <main>
 <div>
 <div class="map">
-<Map bind:selected {colors} />
+<Radar/>
 </div>
-<div>
+<div class="chart-container">
+    <Radar/>
 
 </div>
 </div>
---
+++ frontend/src/lib/_data/radarScores.js
@@ -0,0 +1,21 @@
+export default [
+	{
+		name: 'Allison',
+		fastball: 10,
+		change: 0,
+		slider: 4,
+		cutter: 8,
+		curve: 5
+	},
+
+    {
+        name: 'Bobby',
+        fastball: 8,
+        change: 5,
+        slider: 6,
+        cutter: 7,
+        curve: 4
+    }
+
+
+];
